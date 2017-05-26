b7c49208de24ba8aaa1a464720264104e98aa54c
SamTroth <samt2510@hotmail.co.uk>
2017-05-26 13:53:18 +0100

    26-05-2017 UI show
    Sun down Ui to show when to fire

--- SunDown ShowDown/Assets/Scripts/World/WayPoints.cs
+++ SunDown ShowDown/Assets/Scripts/World/WayPoints.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class WayPoints : MonoBehaviour {
 	public Transform[] patrolPoints;
 	private float moveSpeed;
 	private int currentPoint;
 
-	// Use this for initialization
-	void Start () 
+    public GameObject[] UI;
+
+    private bool shownUi;
+
+    // Use this for initialization
+    void Start () 
 	{
         moveSpeed = Random.Range(0.12f, 0.5f);
 		transform.position = patrolPoints [0].position;
@@ -20,13 +25,20 @@ public class WayPoints : MonoBehaviour {
      if (transform.position == patrolPoints[currentPoint].position)
 		{
 			currentPoint++;
-
         }
 		if (currentPoint >= patrolPoints.Length)
 		{
 			currentPoint = 1;
-		}
-
+            ShowReadyUI();           
+        }
 		transform.position = Vector3.MoveTowards (transform.position, patrolPoints[currentPoint].position, moveSpeed* Time.deltaTime);
 	}
+    void ShowReadyUI()
+    {
+        if (!shownUi)
+        {
+            shownUi = true;
+            UI[Random.Range(0, UI.Length)].SetActive(true);
+        }
+    }
 }
